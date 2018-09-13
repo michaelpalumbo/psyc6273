fd45a15757050014f3c33eee8d8b377ca870ab83
Richard Murray <rfm@yorku.ca>
2018-09-13 17:20:43 -0400

    code

--- 2 - if-else; functions/code1_ifelse.m
+++ 2 - if-else; functions/code1_ifelse.m
@@ -8,47 +8,50 @@ num = input('enter a number between 0 and 1:  ');
 
 % if statement
 if num < 0
-	num = 0;
+	x = 0;
 end
 
 % if-else
 if num < 0
-	num = -1;
+	x = -1;
 else
-	num = 1;
+	x = 1;
 end
 
-% if-else-elseif
+% if-elseif-else
 if num < -10
-	num = -2;
+	x = -2;
 elseif num < 0
-    num = -1;
+    x = -1;
 elseif num == 0
-    num = 0;
+    x = 0;
 elseif num <= 10
-    num = 1;
+    x = 1;
 else
-    num = 2;
+    x = 2;
 end
 
 % nested if-else
 if num < 0
     
     if num < -5
-        num = -2;
+        x = -2;
     else
-        num = -1;
+        x = -1;
     end
     
 else
-    num = 1;
+    x = 1;
 end
 
 % logical operations
+%   && = and
+%   || = or
+%   ~  = not
 a = rand;
 b = rand;
 if ( a>0.5 && b>0.5 ) || ~( a>0.9 )
-	num = 0;
+	x = 0;
 end
 
 % use || && ~ for if-elseif-else (also switch and while)
